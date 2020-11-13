e24980dd8fe0b342be7499a7c8ff087d7ccce917
Lê Văn Quân <quanbka.cntt@gmail.com>
2020-11-13 15:23:22 +0700

    foooter

--- resources/views/frontend/contact/index.blade.php
+++ resources/views/frontend/contact/index.blade.php
@@ -65,41 +65,7 @@
 
     </div>
 
-    <div class="footer-container">
-        <div class="ui container">
-            <a href="#">
-            <img class="footer-logo mb-40 mt-40" src="images/logo.png">
-            </a>
-            <div class="ui equal width grid stackable">
-            <div class="column">
-                <div class="text-20 text-bold text-blue mb-20">Công ty Tài chính Cổ phần Điện lực</div>
-                <div class="text-14 text-bold text-blue">Tầng 14,15 & 16 Tháp B, tòa nhà EVN</div>
-                <div class="text-14  text-blue">11 Cửa Bắc, P. Trúc Bạch, Q. Ba Đình, TP. Hà Nội</div>
-                <div class="text-14  text-blue">Điện thoại: (84-24) 2222.9999</div>
-                <div class="text-14  text-blue">Fax: (84-24) 2222.1999</div>
-                <div class="text-14  text-blue mt-40">Giấy phép số 90/GP-TTĐT cấp ngày 29/08/2008</div>
-
-            </div>
-            <div class="column">
-                <div class="text-20 text-bold text-blue mb-20">Chi nhánh Thành phố Đà Nẵng</div>
-                <div class="text-14  text-blue">Lô A2.12, Đường 30 tháng 4, P. Hoà Cường Bắc, Q. Hải Châu, TP. Đà Nẵng
-                </div>
-                <div class="text-14  text-blue">Điện thoại: (84-236) 222.3456</div>
-                <div class="text-14  text-blue">Fax: (84-236) 222.22409</div>
-
-            </div>
-            <div class="column">
-                <div class="text-20 text-bold text-blue mb-20">Chi nhánh Thành phố Hồ Chí Minh</div>
-                <div class="text-14  text-blue">Lô H (ii) và I (i), Tầng 9, Tòa nhà Sunwah Tower, 115 Đại lộ Nguyễn Huệ,
-                P. Bến Nghé, Q. 1, TP. HCM
-                </div>
-                <div class="text-14  text-blue">Điện thoại: (84-28) 2222.9999</div>
-                <div class="text-14  text-blue">Fax: (84-28) 2222.8999</div>
-
-            </div>
-            </div>
-        </div>
-    </div>
+      @include('frontend.footer')
 
     <script
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBuA-yX_z3esO2vmem94LRPFe4NGE9LLUw&callback=initMap&libraries=&v=weekly"

f4721abf4f6d9b528337670b0f7c8ce151c69714
Le Van Quan <quanbka.cntt@gmail.com>
2020-11-30 10:53:50 +0700

    cors

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 /**
  * Public api
